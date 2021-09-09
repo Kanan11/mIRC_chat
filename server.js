92dde1853734a967aa5362c535585de7ab54c1ce
Kanan <kenan@luxar.se>
2021-09-09 14:02:37 +0200

    Grund Struktur

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+const port = 3000
+
+app.use(express.static('public'))
+
+http.listen(port, ()=>{console.log('listen port' + port)})
+
+io.on('', (socket) => {
+
+    
+})
