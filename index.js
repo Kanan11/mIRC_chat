dd29207f9b4822c6acb21ac26ef84f4709608b8a
Kanan <kenan@luxar.se>
2021-09-13 15:03:32 +0200

    Typing

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
+const { create } = require('domain');
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-server.listen(3007);
+server.listen(3037);
 
-app.get('/', function(request, respons) {
-	respons.sendFile(__dirname + '/public/index.html');
-});
+
 
 connections = [];
 
 io.sockets.on('connection', function(socket) {
 	console.log("Connected");
 	// lägga till ny connection
-	connections.push(socket);
-
+	socket.on('createUser', function(data) {
+		connections.push({id:socket.id, data:data.name, typing:false});
+		
+	})
 	socket.on('disconnect', function(data) {
+		console.log(connections);
 		// Ta bort USER
-		connections.splice(connections.indexOf(socket), 1);
+		
+		connections.splice(socket.id.indexOf(socket), 1);
 		console.log("Disconnected");
 	});
-
-	// To mot meddelande
+	
+	// Ta mot meddelande
 	socket.on('send mess', function(data) {
 		// inne func vi skickar ny data 'add mess',
 		// det data ska synas i varie user 
-		io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+		io.sockets.emit('add mess', {id: data.id, mess: data.mess, name: data.name, className: data.className});
 	});
-
+	
 	
 	socket.on('typing', (data)=>{
+		console.log("typing");
 		if(data.typing==true)
-		   io.emit('display', data)
+		io.emit('display', data)
 		else
-		   io.emit('display', data)
-	  })
-
+		io.emit('display', data)
+	})
+	
 });
+
+app.use(express.static("public"))
