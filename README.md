0df518514351ed4f776d29603e52b298a6d78b87
Kanan <kenan@luxar.se>
2021-09-17 14:02:47 +0200

    Final

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # mIRC_chat
 Chat app powered by Express 
+https://github.com/Kanan11/mIRC_chat
 
 Server körs på port: 3037.
 Server fil heter index.js.
@@ -7,13 +8,12 @@ Klient-kod ligger i public mappen.
 
 På vår klientsida har vi använt oss av Bootstrap och JQuery.
 
-1. Installera express och socket.io. "npm install express" & "npm install socket.io".
-2. För att köra igång skriver vi "npm start" eller "node index.js" och går till http://localhost:3037 i webbläsaren.
-3. För att kunna chatta med flera fönster mot varandra. Så öppnar vi localhost 3037 i flera separata fönster. 
-4. Anger ett namn, skriver ett meddelande och skickar.
+1. Installera express och socket.io. "npm install"
+2. För att köra igång skriver vi "npm start" eller "nodemon index.js" och öppna http://localhost:3037 i webbläsaren.
+3. För att kunna chatta med flera fönster mot varandra. Så öppnar vi localhost med 3037 port i flera separata fönster. 
+4. Anger ett namn, skriver ett meddelande och tryck på "SKICKA" knappen.
+5. I message input fältet om man skriver "/" och trycker på "/quote" då hämtas API text 
 
 Varje användare får en separat färg som hämtas ifrån Bootstrap.
-När någon lämnar chatten så ser man det i ett meddelande.
-När någon skriver ett meddelande så visas det "User is typing" under chatten.
-
-i message input fältet om skriver "/" om man välja "/quote" då kan hämta API text 
+När någon ansluter eller lämnar chatten så ser man det i ett meddelande.
+När någon skriver ett meddelande så visas det "User is typing" under chatten inom 1,5 sec timeout.
--- index.js
+++ index.js
@@ -13,42 +13,19 @@ connections = [];
 io.sockets.on("connection", function (socket) {
   console.log("Connected");
   let userName = null;
+
   // -------- Ta emot data från klient och skicka till alla användare vem som anslutit ------------
   socket.on("new-user", (name) => {
-    //userName = name;
     connections.push({ id: socket.id, userName: name, typing: false })
     users[socket.id] = userName;
-    //connections.forEach(element => console.log(element));
-
     socket.broadcast.emit("user-connected", name);
-    //console.log(connections.valueOf(userName))
-    //console.log(connections[0].userName)
-    /* if(name === connections[0].userName){
-      
-      console.log('bingo')
-    }else{
-    } */
-    /* connections.forEach((userName) => {
-      console.log(userName);
-    }); */
-    //console.log(users + " users1");
-    //console.log(users, ' users2');
-
-
-    /* console.log(userName, ' userName');
-    console.log(name, ' name'); */
-    //console.log(connections, ' connections');
-    //console.log(Object.keys(connections), connections[0].userName);
-    //console.log(socket, ' socket');
   });
-
 
 
   socket.on("disconnect", function (data) {
     io.sockets.emit("user-disconnected", userName);
-    /* 		socket.broadcast.emit('user-connected', userName); */
-    // Ta bort USER
 
+    // Ta bort USER
     connections.splice(socket.id.indexOf(socket), 1);
     console.log("Disconnected");
   });
@@ -57,8 +34,6 @@ io.sockets.on("connection", function (socket) {
   socket.on("send mess", function (data) {
     // inne func vi skickar ny data 'add mess',
     // det data ska synas i varie user
-    //console.log('he is sending message now '+ data.name);
-    console.log(data)
     io.sockets.emit("add mess", {
       mess: data.mess,
       name: userName,
@@ -68,15 +43,8 @@ io.sockets.on("connection", function (socket) {
 
   socket.on("typing", (data) => {
     userName = data.name;
-    //console.log("ontyping", data);
     io.emit("display", data);
   });
-
-  /* socket.on('send mess', function (data) {
-    io.sockets.emit('send mess', data);
-  }); */
-
-
 
 });
 
